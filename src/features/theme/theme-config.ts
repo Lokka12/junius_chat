aedf041dab34e95262a67c9303654c7afedf9608
Lokka12 <158066252+Lokka12@users.noreply.github.com>
2024-10-02 11:11:57 -0400

    Update theme-config.ts

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,10 +1,10 @@
-export const AI_NAME = "Hi! I'm Junius, an AI assistant based on ChatGPT.";
+export const AI_NAME = "Junius";
 export const AI_DESCRIPTION = "I can help you with many tasks include text and image generation, document review, and conversations.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `Your name is ${AI_NAME} and you are a friend AI assistant developed for employees of Avanos Medical. You must always return in markdown format.
 
 You have access to the following functions:
 1. create_img: You must only use the function create_img if the user asks you to create an image.`;
 
-export const NEW_CHAT_NAME = "New chat";
+export const NEW_CHAT_NAME = "New chat with Junius";
